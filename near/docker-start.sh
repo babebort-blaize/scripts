cb3de5ae4ff291726f27091e15eec5365cd7bcd9
babebort-blaize <109084492+babebort-blaize@users.noreply.github.com>
2022-08-26 11:01:56 +0100

    Update docker-start.sh

--- near/docker-start.sh
+++ near/docker-start.sh
@@ -12,24 +12,18 @@ sudo apt-get install -y \
     lsb-release wget
 
 sudo mkdir -p /etc/apt/keyrings && \
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg 
---dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) 
-signed-by=/etc/apt/keyrings/docker.gpg] 
-https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee 
-/etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update -y && sudo apt install docker-ce docker-ce-cli 
-containerd.io docker-compose-plugin httpie -y
+sudo apt-get update -y && sudo apt install docker-ce docker-ce-cli \
+    containerd.io docker-compose-plugin httpie -y
 
-docker run -v $HOME/.near:/root/.near -p 3030:3030 -d --name nearup 
-nearup/nearprotocol run localnet
+docker run -v $HOME/.near:/root/.near -p 3030:3030 -d --name nearup nearup/nearprotocol run localnet
 
-sleep 10
+sleep 20
 
-http post http://127.0.0.1:3030  jsonrpc=2.0 id=dontcare method=tx     
-params:='[ "6zgh2u9DqHHiXzdy9ouTP7oGky2T4nugqzqt9wJZwNFm", 
-"sender.mainnet"]'
+http post http://127.0.0.1:3030  jsonrpc=2.0 id=dontcare method=tx \
+   params:='[ "6zgh2u9DqHHiXzdy9ouTP7oGky2T4nugqzqt9wJZwNFm", "sender.mainnet"]'
