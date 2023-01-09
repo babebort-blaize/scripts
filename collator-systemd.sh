9d1b98cdb1538f321883bc4b2120162cc2d47ff4
babebort-blaize <109084492+babebort-blaize@users.noreply.github.com>
2023-01-09 20:37:46 +0000

    Update collator-systemd.sh

--- collator-systemd.sh
+++ collator-systemd.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-NODE_NAME="BLAIZE-NODE-TEST"
+NODE_NAME="BLAIZE-NODE"
 
 
 sudo apt-get update -y 
@@ -12,17 +12,6 @@ sudo apt-get install -y \
     curl \
     gnupg \
     lsb-release wget
-
-sudo mkdir -p /etc/apt/keyrings && \
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -y && sudo apt install docker-ce docker-ce-cli \
-    containerd.io docker-compose-plugin httpie -y
-
 
 wget $(curl -s https://api.github.com/repos/AstarNetwork/Astar/releases/latest | grep "tag_name" | awk '{print "https://github.com/AstarNetwork/Astar/releases/download/" substr($2, 2, length($2)-3) "/astar-collator-v" substr($2, 3, length($2)-4) "-ubuntu-x86_64.tar.gz"}')
 
@@ -41,7 +30,7 @@ Group=astar
 ExecStart=/usr/local/bin/astar-collator \
   --collator \
   --rpc-cors all \
-  --name BLAIZE-NODE \
+  --name ${NODE_NAME} \
   --chain astar \
   --base-path /var/lib/astar \
   --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
@@ -53,5 +42,6 @@ RestartSec=120
 [Install]
 WantedBy=multi-user.target' > /etc/systemd/system/astar.service
 
-sudo systemctl enabled astar
-sudo systemctl start astar.service
+systemctl enabled astar
+systemctl start astar.service
+systemctl status astar
