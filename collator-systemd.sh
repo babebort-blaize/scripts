b22cedd1f6ae800c0ea26703d2076e731c10b3ac
babebort-blaize <109084492+babebort-blaize@users.noreply.github.com>
2023-01-09 20:36:15 +0000

    Update collator-systemd.sh

--- collator-systemd.sh
+++ collator-systemd.sh
@@ -22,3 +22,36 @@ echo \
 
 sudo apt-get update -y && sudo apt install docker-ce docker-ce-cli \
     containerd.io docker-compose-plugin httpie -y
+
+
+wget $(curl -s https://api.github.com/repos/AstarNetwork/Astar/releases/latest | grep "tag_name" | awk '{print "https://github.com/AstarNetwork/Astar/releases/download/" substr($2, 2, length($2)-3) "/astar-collator-v" substr($2, 3, length($2)-4) "-ubuntu-x86_64.tar.gz"}')
+
+
+tar -xvf astar-collator*.tar.gz
+
+mv astar-collator /usr/local/bin/astar-collator
+
+echo '[Unit]
+Description=Astar Collator
+
+[Service]
+User=astar
+Group=astar
+  
+ExecStart=/usr/local/bin/astar-collator \
+  --collator \
+  --rpc-cors all \
+  --name BLAIZE-NODE \
+  --chain astar \
+  --base-path /var/lib/astar \
+  --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
+  --execution wasm
+
+Restart=always
+RestartSec=120
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/astar.service
+
+sudo systemctl enabled astar
+sudo systemctl start astar.service
